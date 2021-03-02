703651a7b3d6c98c4772109926367d2429f9a915
Sandeepdevops0412 <sandeepdevops0412@gmail.com>
2021-03-02 19:45:08 +0530

    third commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Manish'),
+        body: JSON.stringify('Hello from sandeep'),
     };
     return response;
 };
