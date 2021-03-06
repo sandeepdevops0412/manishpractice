691f778190c842c537725754e6ba104f8925876d
Sandeepdevops0412 <sandeepdevops0412@gmail.com>
2021-03-06 15:22:49 +0530

    fifth commit

--- b/index.js
+++ b/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from sandeep'),
+        body: JSON.stringify('Hello from lasya'),
     };
     return response;
 };
